01a23c3949deda287513643eed2659652d055508
Daniel Liebner <dliebner@gmail.com>
2021-10-10 23:44:57 -0400

    prog

--- common.php
+++ common.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);
 error_reporting( E_ERROR | E_WARNING | E_PARSE | E_RECOVERABLE_ERROR );
 
 // autoload.php
-require_once( $root_path. '/vendor/autoload.php' );
+require_once( $root_path. 'vendor/autoload.php' );
 
 // Includes
 require_once( $root_path. 'includes/functions.php' );
