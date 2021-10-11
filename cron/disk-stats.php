07590843f3fa16f9483970950aa3159a8fcca586
Daniel Liebner <dliebner@gmail.com>
2021-10-11 01:39:50 -0400

    p

--- cron/disk-stats.php
+++ cron/disk-stats.php
@@ -4,6 +4,9 @@ define('IN_SCRIPT', 1);
 
 $root_path = '/home/bgcdn/';
 
+// Script runs once per minute, we sleep to avoid lots of on-the-minute processing
+sleep(5);
+
 require($root_path . 'common.php');
 
 $db = db();
