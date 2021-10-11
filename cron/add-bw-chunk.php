dff2685db81a07d8c19cf6834020dbce2f5d4ae2
Daniel Liebner <dliebner@gmail.com>
2021-10-10 23:51:43 -0400

    p

--- cron/add-bw-chunk.php
+++ cron/add-bw-chunk.php
@@ -17,18 +17,20 @@ $ret = $redis->multi()
 	->del('bgcdn:bw_chunk')
 	->exec();
 
-$chunkBytes = (int)$ret[0];
+if( $chunkBytes = (int)$ret[0] ) {
 
-// Add chunk bytes to running total
-$sql = "INSERT INTO bandwidth_logs (`month`, bytes_out)
-SELECT * FROM (
-    SELECT LAST_DAY(NOW() - INTERVAL 1 MONTH) + INTERVAL 1 DAY as `month`, $chunkBytes as bytes_out
-) as aux
-ON DUPLICATE KEY UPDATE
-	bandwidth_logs.bytes_out = bandwidth_logs.bytes_out + aux.bytes_out";
+	// Add chunk bytes to running total
+	$sql = "INSERT INTO bandwidth_logs (`month`, bytes_out)
+	SELECT * FROM (
+		SELECT LAST_DAY(NOW() - INTERVAL 1 MONTH) + INTERVAL 1 DAY as `month`, $chunkBytes as bytes_out
+	) as aux
+	ON DUPLICATE KEY UPDATE
+		bandwidth_logs.bytes_out = bandwidth_logs.bytes_out + aux.bytes_out";
 
-if( !$db->sql_query($sql) ) {
+	if( !$db->sql_query($sql) ) {
 
-	throw new QueryException("Error inserting", $sql);
+		throw new QueryException("Error inserting", $sql);
+
+	}
 
 }
