5d3a65020ef6beb162ec22cea67643f326bd3d81
Daniel Liebner <dliebner@gmail.com>
2021-10-11 01:18:28 -0400

    p

--- cron/update-30s-bw.php
+++ cron/update-30s-bw.php
@@ -15,10 +15,10 @@ $start = time();
 set_time_limit(60 * 2);
 while( time() - 60 < $start ) {
 
-	$bw30s = (int)$redis->get('bgcdn:bw_30sec_exp_' . (time() + 1));
+	$bytes30s = (int)$redis->get('bgcdn:bw_30sec_exp_' . (time() + 1));
 
-	ServerStatus::set('avg_bytes_per_sec_30s', round($bw30s / 30) );
-	ServerStatus::set('port_saturation', round($bw30s * 8 / 30 / Config::get('port_speed_bps'), 4) );
+	ServerStatus::set('avg_bytes_per_sec_30s', round($bytes30s / 30) );
+	ServerStatus::set('port_saturation', round($bytes30s * 8 / 30 / Config::get('port_speed_bps'), 4) );
 
 	sleep(1);
 
