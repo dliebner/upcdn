c5ef12056f07de17b485dac107e88f1c2a460f3a
Daniel Liebner <dliebner@gmail.com>
2021-10-15 14:49:47 -0400

    p

--- devel/b2-test.php
+++ devel/b2-test.php
@@ -7,9 +7,9 @@ require_once( $root_path. 'vendor/autoload.php' );
 use obregonco\B2\Client;
 use obregonco\B2\Bucket;
 
-$client = new Client('002ef553d27505e0000000001', [
-	//'keyId' => '', // optional if you want to use master key (account Id)
-	'applicationKey' => 'K002WhVSExFkgz27fDhfwfGGJrBoWKk',
+$client = new Client(Config::get('b2_master_key_id'), [
+	'keyId' => Config::get('b2_application_key_id'), // optional if you want to use master key (account Id)
+	'applicationKey' => Config::get('b2_application_key'),
 ]);
 $client->version = 2; // By default will use version 1
 
