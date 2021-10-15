1652a559773d717f3cdbc804d91556b224098f25
Daniel Liebner <dliebner@gmail.com>
2021-10-15 17:09:25 -0400

    p

--- devel/validateKey.php
+++ devel/validateKey.php
@@ -7,7 +7,9 @@ $root_path = './../';
 require_once( $root_path. 'common.php' );
 
 CDNClient::postToHub(CDNClient::HUB_ACTION_VALIDATE_KEY, [], [
-	'serverId' => $newServerId,
-	'secretKey' => $newSecretKey,
-	'hubApiUrl' => $newHubApiUrl,
+	'success' => function() {
+
+		echo "Secret key validated.";
+
+	}
 ]);
