99d9cf91f18f41f017db06c3eb15aab4763ba1e6
Daniel Liebner <dliebner@gmail.com>
2021-10-16 01:44:55 -0400

    p

--- devel/b2-test.php
+++ devel/b2-test.php
@@ -11,19 +11,13 @@ error_reporting(E_ALL);
 use obregonco\B2\Client;
 use obregonco\B2\Bucket;
 
-echo __LINE__ . "\n";
-
 $client = new Client(Config::get('b2_master_key_id'), [
 	'keyId' => Config::get('b2_application_key_id'), // optional if you want to use master key (account Id)
 	'applicationKey' => Config::get('b2_application_key'),
 ]);
 $client->version = 2; // By default will use version 1
 
-echo __LINE__ . "\n";
-
 // Retrieve an array of Bucket objects on your account.
 $buckets = $client->listBuckets();
-
-echo __LINE__ . "\n";
 
 print_r($buckets);
