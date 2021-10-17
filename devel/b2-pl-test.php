c29fb93de70fb3f971d9e058e20efb2e596d387b
Daniel Liebner <dliebner@gmail.com>
2021-10-17 12:25:10 -0400

    p

---
+++ devel/b2-pl-test.php
@@ -0,0 +1,41 @@
+<?php
+
+define('IN_SCRIPT', 1);
+
+$root_path = './../';
+
+require_once( $root_path. 'common.php' );
+
+error_reporting(E_ALL);
+
+use obregonco\B2\Client;
+use obregonco\B2\Bucket;
+use obregonco\B2\ParallelUploader;
+
+$client = new Client(Config::get('b2_master_key_id'), [
+	'keyId' => Config::get('b2_application_key_id'), // optional if you want to use master key (account Id)
+	'applicationKey' => Config::get('b2_application_key'),
+]);
+$client->version = 2; // By default will use version 1
+
+$bucketId = $client->getBucketFromName('bidglass-creatives')->getId();
+
+$pup = new ParallelUploader($client, $bucketId);
+
+$pup->addFileToUpload([
+    'FileName' => 'test/test.txt',
+    'Body' => fopen('./test.txt', 'r')
+]);
+
+for( $i = 1; $i <= 10; $i++ ) {
+
+    $pup->addFileToUpload([
+        'FileName' => "test/test$i.txt",
+        'Body' => fopen("./test$i.txt", 'r')
+    ]);
+
+}
+
+$files = $pup->doUpload();
+
+print_r($files);
