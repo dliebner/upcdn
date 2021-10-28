fe9daba211b4cf45c8c2f697ec11ed1298fbfbb3
Daniel Liebner <dliebner@gmail.com>
2021-10-27 23:55:28 -0400

    p

--- www/client-api/index.php
+++ www/client-api/index.php
@@ -7,6 +7,90 @@ $root_path = './../../';
 include_once( $root_path . 'common.php' );
 require_once( $root_path . 'includes/JSONEncrypt.php');
 
+function debugEnabled() {
+
+	return in_array($_SERVER['REMOTE_ADDR'], explode(",", Config::get('debug_ips')));
+
+}
+
+function default_exception_handler($e) {
+	
+	$eClass = get_class($e);
+	
+	if( in_array($eClass, array('QueryException','GeneralException','SilentAjaxException')) ) {
+	
+		handleAjaxException($e);
+		
+	} else {
+
+		echo '<div>';
+		echo '<b>Fatal error</b>:  Uncaught exception \'' . get_class($e) . '\' with message ';
+		echo $e->getMessage() . '<br>';
+		echo 'Stack trace:<pre>' . $e->getTraceAsString() . '</pre>';
+		echo 'thrown in <b>' . $e->getFile() . '</b> on line <b>' . $e->getLine() . '</b><br>';
+		echo '</div>';
+		
+	}
+
+}
+
+function handleAjaxException(Exception $e, $options = array()) {
+		
+	switch( get_class($e) ) {
+
+		case 'GeneralException':
+
+			AjaxResponse::returnError($e->getMessage(), null, $options);
+
+		case 'GeneralExceptionWithData':
+
+			AjaxResponse::returnError($e->getMessage(), $e->data, $options);
+			
+		case 'SilentAjaxException':
+
+			die( AjaxResponse::status('silentError', $e->getMessage(), null, $options) );
+		
+		case 'QueryException':
+
+			global $db;
+
+			if( debugEnabled() ) {
+			
+				AjaxResponse::criticalDie(
+					$e->getMessage() . "\n"
+						. 'on line ' . $e->getLine() . "\n"
+						. ' in ' . $e->getFile() . "\n\n"
+						. $e->sql,
+					array('sql' => $e->sql, 'err' => $db->sql_error()),
+					$options
+				);
+
+			} else {
+			
+				AjaxResponse::criticalDie(
+					$e->getMessage() . "\n"
+						. 'on line ' . $e->getLine() . "\n"
+						. ' in ' . $e->getFile(),
+					[],
+					$options
+				);
+
+			}
+			
+			break;
+			
+		default:
+
+			AjaxResponse::criticalDie($e->getMessage(), null, $options);
+			
+			break;
+		
+	}
+	
+}
+
+set_exception_handler('default_exception_handler');
+
 /**
  * 	Client API
  * 		Actions require tokens granted by Hub server
@@ -238,7 +322,7 @@ switch( $action ) {
 
 		}
 
-		AjaxResponse::returnError("Error encoding video.", in_array($_SERVER['REMOTE_ADDR'], explode(",", Config::get('debug_ips'))) ? [
+		AjaxResponse::returnError("Error encoding video.", debugEnabled() ? [
 			'cmd' => $cmd,
 			'execResult' => $execResult,
 			'execOutput' => $execOutput,
