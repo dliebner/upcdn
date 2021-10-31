e5f78c151080e9d3ba250b7ad26787fd7832dd43
Daniel Liebner <dliebner@gmail.com>
2021-10-31 05:41:27 -0400

    p

--- daemon/docker-events.sh
+++ daemon/docker-events.sh
@@ -4,18 +4,6 @@ docker events --filter 'type=container' --filter 'event=start' --filter 'event=s
 
 do
   
-    container_id=`echo $event | sed 's/.*Z\ \(.*\):\ .*/\1/'`
-
-    echo $container_id >> log.txt
-
-    ipaddress=`docker inspect --format='{{.NetworkSettings.IPAddress}}' $container_id`
-    port=`docker inspect --format='{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' $container_id`
-    domain=`docker inspect --format='{{.Config.Domainname}}' $container_id`
-    host=`docker inspect --format='{{.Config.Hostname}}' $container_id`
-    
-    echo $ipaddress >> log.txt
-    echo $port >> log.txt
-    echo $host.$domain >> log.txt
-	echo
+    echo $event >> log.txt
 
 done
