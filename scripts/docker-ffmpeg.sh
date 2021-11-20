8ac728ab542db50f5cd4406418f369a4dcd7b260
Daniel Liebner <dliebner@gmail.com>
2021-11-20 11:17:00 -0500

    p

--- scripts/docker-ffmpeg.sh
+++ scripts/docker-ffmpeg.sh
@@ -2,18 +2,18 @@
 
 while getopts d:i:o:b:w:h:st:pm flag
 do
-    case "${flag}" in
-        d) dir=${OPTARG};;
-        i) inFile=${OPTARG};;
-        o) outFile=${OPTARG};;
-        b) bitRate=${OPTARG};;
-        w) constrainWidth=${OPTARG};;
-        h) constrainHeight=${OPTARG};;
-        s) hlsOutput=1;;
-        t) hlsTime=${OPTARG};;
-        p) passthrough=1;;
-        m) mute=1;;
-    esac
+	case "${flag}" in
+		d) dir=${OPTARG};;
+		i) inFile=${OPTARG};;
+		o) outFile=${OPTARG};;
+		b) bitRate=${OPTARG};;
+		w) constrainWidth=${OPTARG};;
+		h) constrainHeight=${OPTARG};;
+		s) hlsOutput=1;;
+		t) hlsTime=${OPTARG};;
+		p) passthrough=1;;
+		m) mute=1;;
+	esac
 done
 
 dirParams=()
@@ -28,35 +28,36 @@ encodeParams=()
 
 if [ ! -z "$passthrough" ] && [ -z "$hlsOutput" ]; then
 	# Passthrough
-    encodeParams+=( -c:v copy )
+	encodeParams+=( -c:v copy )
 else
-    # Not Passthrough
-    encodeParams+=( -c:v h264 )
-    encodeParams+=( -b:v "$bitRate" -preset medium )
-    encodeParams+=( -vf "select='eq(n,0)+if(gt(t-prev_selected_t,1/30.50),1,0)'",scale="$constrainWidth:$constrainHeight" )
-    encodeParams+=( -sws_flags bicubic )
-    encodeParams+=( -movflags +faststart -pix_fmt yuv420p )
+	# Not Passthrough
+	encodeParams+=( -c:v h264 )
+	encodeParams+=( -b:v "$bitRate" -preset medium )
+	encodeParams+=( -vf "select='eq(n,0)+if(gt(t-prev_selected_t,1/30.50),1,0)'",scale="$constrainWidth:$constrainHeight" )
+	encodeParams+=( -sws_flags bicubic )
+	encodeParams+=( -movflags +faststart -pix_fmt yuv420p )
 fi
 
 if [ ! -z "$mute" ]; then
-    # Mute
+	# Mute
 	encodeParams+=( -an )
 else
-    # Audio encode
-    encodeParams+=( -c:a libfdk_aac )
-    encodeParams+=( -vbr 2 )
+	# Audio encode
+	encodeParams+=( -c:a libfdk_aac )
+	encodeParams+=( -vbr 2 )
+	encodeParams+=( -profile:a aac_he )
 fi
 
 if [ ! -z "$hlsOutput" ]; then
-    if [ -z "$hlsTime" ]; then
-        # aiming for 400k chunks by default
-        divide=400000; (( by=bitRate/8 )); (( hlsTime=(divide+by-1)/by ))
-    fi
-    if [ $hlsTime -lt 2 ]; then
-        hlsInitTime=1
-    else
-        hlsInitTime=2
-    fi
+	if [ -z "$hlsTime" ]; then
+		# aiming for 400k chunks by default
+		divide=400000; (( by=bitRate/8 )); (( hlsTime=(divide+by-1)/by ))
+	fi
+	if [ $hlsTime -lt 2 ]; then
+		hlsInitTime=1
+	else
+		hlsInitTime=2
+	fi
 	encodeParams+=( -f hls )
 	encodeParams+=( -hls_playlist_type vod )
 	encodeParams+=( -hls_init_time "$hlsInitTime" )
