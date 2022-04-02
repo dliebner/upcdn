327634079e14ceac162947fb8add21c8e64722f7
Daniel Liebner <dliebner@gmail.com>
2022-04-02 02:57:30 -0400

    Minor install script updates

--- install/install.sh
+++ install/install.sh
@@ -19,16 +19,16 @@ chmod +x ../daemon/*
 #chmod +x ../cron/*.sh
 chmod +x ../devel/cpp-redis-pipe/*.sh
 
-# webmin repo + key
-echo "deb http://download.webmin.com/download/repository sarge contrib" | sudo tee /etc/apt/sources.list.d/webmin.list > /dev/null
-wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add
-
 # general dependencies
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python unzip ufw zip g++ make cmake git
 
+# webmin repo + key
+echo "deb http://download.webmin.com/download/repository sarge contrib" | sudo tee /etc/apt/sources.list.d/webmin.list > /dev/null
+wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add
+
 #install webmin
-sudo apt install webmin
+sudo apt update && sudo apt install webmin
 
 # docker repo + key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -108,8 +108,8 @@ sudo apt install php-mysql phpmyadmin
 
 # force SSL on phpMyAdmin
 PMAACONF=/etc/phpmyadmin/apache.conf
-cp $PMAACONF ${PMAACONF}.original
-sed -i '/<Directory \/usr\/share\/phpmyadmin>/a\
+sudo cp $PMAACONF ${PMAACONF}.original
+sudo sed -i '/<Directory \/usr\/share\/phpmyadmin>/a\
 \
     # bgcdn config - force SSL\
     RewriteEngine On\
