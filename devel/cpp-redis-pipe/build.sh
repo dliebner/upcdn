9fd93af7b1b71ca83d31ac4fa6a98d826ab5a339
Daniel Liebner <dliebner@gmail.com>
2023-05-09 05:20:58 -0400

    Fix cpp_redis build (tacopie issues)

--- devel/cpp-redis-pipe/build.sh
+++ devel/cpp-redis-pipe/build.sh
@@ -7,6 +7,10 @@ rm -rf cpp_redis lib; mkdir lib
 (  git clone https://github.com/cpp-redis/cpp_redis
    cd cpp_redis
    git submodule init; git submodule update
+   cd tacopie
+   git fetch origin pull/5/head:cmake-fixes
+   git checkout cmake-fixes
+   cd ..
    mkdir build; cd build
    cmake .. -D{CMAKE_BUILD_TYPE=Release,CMAKE_INSTALL_PREFIX=../../lib}
    make
